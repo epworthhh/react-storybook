0bd5e0c098be525aad4bf1bd1cd61c0bd3fa5ec2
Héctor Figueroa <hectorfigueroa@eldiario.es>
2018-09-26 18:00:34 +0200

    Book story

---
+++ src/Button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button } from './Button';
+
+storiesOf('Button', module)
+  .addWithJSX('with background', () => (
+    <Button bg="palegoldenrod">Hello world</Button>
+  ))
+  .addWithJSX('with background 2', () => (
+    <Button bg="green">Hello world 2</Button>
+  ));
