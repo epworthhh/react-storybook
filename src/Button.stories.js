12770c751ff7028a0f72c96f07a4cd19e2b5a7d5
Héctor Figueroa <hectorfigueroa@eldiario.es>
2018-09-27 16:30:47 +0200

    add addon-info & addon-knobs

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,11 +1,21 @@
 import React from 'react';
+
+import { wInfo } from './utils';
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
+import { text, color } from '@storybook/addon-knobs/react';
 
 storiesOf('Button', module)
-  .addWithJSX('with background', () => (
-    <Button bg="palegoldenrod">Hello world</Button>
-  ))
+  .addWithJSX(
+    'with background', 
+    wInfo(`
+      description of the component
+
+      ~~~js
+        <Button>Click here</Button>
+      ~~~
+    `)(() => <Button bg={text('bg', 'palegoldenrod')}>Hello world</Button>)
+  )
   .addWithJSX('with background 2', () => (
-    <Button bg="green">Hello world 2</Button>
+    <Button bg={color('bg', 'green', 'group1')}>Hello world 2</Button>
   ));
