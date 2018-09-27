f146b4c2a9d5e067d5f718c028bc970dc638d508
Héctor Figueroa <hectorfigueroa@eldiario.es>
2018-09-27 17:31:37 +0200

    add TypeScript support

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { wInfo } from './utils';
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
-import { text, color } from '@storybook/addon-knobs/react';
+import { text, color, boolean } from '@storybook/addon-knobs/react';
 
 storiesOf('Button', module)
   .addWithJSX(
@@ -17,5 +17,12 @@ storiesOf('Button', module)
     `)(() => <Button bg={text('bg', 'palegoldenrod')}>Hello world</Button>)
   )
   .addWithJSX('with background 2', () => (
-    <Button bg={color('bg', 'green', 'group1')}>Hello world 2</Button>
+    <Button bg={color('bg', 'green', 'group1')}>
+      Hello world 2
+    </Button>
+  ))
+  .addWithJSX('with background 3', () => (
+    <Button disabled={boolean('disabled', false)}>
+      {text('text', 'Hello world')}
+    </Button>
   ));
